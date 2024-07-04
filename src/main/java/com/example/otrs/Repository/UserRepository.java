1b128f11e014c0ea9df9b9b4035df97ba68f1c90
ishani.s <ishanisamaraweeara@gmail.com>
2024-07-04 17:09:45 +0530

    getting user details from the database

--- src/main/java/com/example/otrs/Repository/UserRepository.java
+++ src/main/java/com/example/otrs/Repository/UserRepository.java
@@ -6,8 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
-
 public interface UserRepository extends JpaRepository<Ticket, String> {
     @Query("SELECT u FROM User U WHERE u.username =  :username")
     User getUserDetails(@Param("username") int username);

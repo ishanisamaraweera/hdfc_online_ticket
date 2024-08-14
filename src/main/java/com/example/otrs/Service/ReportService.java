b64d635f3b35d0569b1dfec68ddf34a465127f5a
ishani.s <ishanisamaraweeara@gmail.com>
2024-08-14 16:18:08 +0530

    Add generate reports

--- src/main/java/com/example/otrs/Service/ReportService.java
+++ src/main/java/com/example/otrs/Service/ReportService.java
@@ -8,7 +8,6 @@ import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
